5445a7d9932fbecd6ad77eae1ea04c92d8c55509
Mike Hagerty <mike.hagerty@heapanalytics.com>
2022-09-26 16:57:46 -0400

    Changed titles

--- src/components/Main.js
+++ src/components/Main.js
@@ -10,7 +10,7 @@ const Main = () => {
 <meta charSet="UTF-8" />
 <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<title>Play | SaaS-Based Admin UI</title>
+<title>Play | Have fun and Play!</title>
 <link
   rel="shortcut icon"
   href="/images/marketing/favicon.png"
