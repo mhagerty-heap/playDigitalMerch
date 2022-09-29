28950f4965d5bd2efd0586f55f38e361d9e63241
Mike Hagerty <mike.hagerty@heapanalytics.com>
2022-09-29 14:08:10 -0400

    Change from <div className="col-12 md:col-6"> to <div className="col-fixed"> on linr 504

--- src/components/MenuDemo.js
+++ src/components/MenuDemo.js
@@ -501,7 +501,7 @@ const MenuDemo = () => {
         <div className="grid p-fluid">
 
 
-            <div className="col-12 md:col-6">
+            <div className="col-fixed">
                 <div className="card card-w-title">
                     <h5>How to Use Play Product Management</h5>
                     <Steps model={wizardItems} activeIndex={activeIndex} onSelect={(e) => setActiveIndex(e.index)} readOnly={false} />
