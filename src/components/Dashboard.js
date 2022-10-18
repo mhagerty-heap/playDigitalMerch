68e49bd6c8b80bc9b9297d61bd0180e6e9f9c901
Mike Hagerty <mike.hagerty@heapanalytics.com>
2022-10-18 16:23:53 -0400

    Changed dashboard percentage and dollar signs

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -10,7 +10,7 @@ const lineData = {
     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
     datasets: [
         {
-            label: 'First Dataset',
+            label: 'Last Year',
             data: [65, 59, 80, 81, 56, 55, 40],
             fill: false,
             backgroundColor: '#2f4860',
@@ -18,7 +18,7 @@ const lineData = {
             tension: .4
         },
         {
-            label: 'Second Dataset',
+            label: 'This Year',
             data: [28, 48, 40, 19, 86, 27, 90],
             fill: false,
             backgroundColor: '#00bb7e',
@@ -137,13 +137,13 @@ const Dashboard = (props) => {
                     <div className="flex justify-content-between mb-3">
                         <div>
                             <span className="block text-500 font-medium mb-3">Revenue</span>
-                            <div className="text-900 font-medium text-xl">$2.100</div>
+                            <div className="text-900 font-medium text-xl">$2,100</div>
                         </div>
                         <div className="flex align-items-center justify-content-center bg-orange-100 border-round" style={{width: '2.5rem', height: '2.5rem'}}>
                             <i className="pi pi-map-marker text-orange-500 text-xl"/>
                         </div>
                     </div>
-                    <span className="text-green-500 font-medium">%52+ </span>
+                    <span className="text-green-500 font-medium">+52% </span>
                     <span className="text-500">since last week</span>
                 </div>
             </div>
@@ -210,7 +210,7 @@ const Dashboard = (props) => {
                                 <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style={{height: '8px'}}>
                                     <div className="bg-orange-500 h-full" style={{width: '50%'}}/>
                                 </div>
-                                <span className="text-orange-500 ml-3 font-medium">%50</span>
+                                <span className="text-orange-500 ml-3 font-medium">50%</span>
                             </div>
                         </li>
                         <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
@@ -222,7 +222,7 @@ const Dashboard = (props) => {
                                 <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style={{height: '8px'}}>
                                     <div className="bg-cyan-500 h-full" style={{width: '16%'}}/>
                                 </div>
-                                <span className="text-cyan-500 ml-3 font-medium">%16</span>
+                                <span className="text-cyan-500 ml-3 font-medium">16%</span>
                             </div>
                         </li>
                         <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
@@ -234,7 +234,7 @@ const Dashboard = (props) => {
                                 <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style={{height: '8px'}}>
                                     <div className="bg-pink-500 h-full" style={{width: '67%'}}/>
                                 </div>
-                                <span className="text-pink-500 ml-3 font-medium">%67</span>
+                                <span className="text-pink-500 ml-3 font-medium">67%</span>
                             </div>
                         </li>
                         <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
@@ -246,7 +246,7 @@ const Dashboard = (props) => {
                                 <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style={{height: '8px'}}>
                                     <div className="bg-green-500 h-full" style={{width: '35%'}}/>
                                 </div>
-                                <span className="text-green-500 ml-3 font-medium">%35</span>
+                                <span className="text-green-500 ml-3 font-medium">35%</span>
                             </div>
                         </li>
                         <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
@@ -258,7 +258,7 @@ const Dashboard = (props) => {
                                 <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style={{height: '8px'}}>
                                     <div className="bg-purple-500 h-full" style={{width: '75%'}}/>
                                 </div>
-                                <span className="text-purple-500 ml-3 font-medium">%75</span>
+                                <span className="text-purple-500 ml-3 font-medium">75%</span>
                             </div>
                         </li>
                         <li className="flex flex-column md:flex-row md:align-items-center md:justify-content-between mb-4">
@@ -270,7 +270,7 @@ const Dashboard = (props) => {
                                 <div className="surface-300 border-round overflow-hidden w-10rem lg:w-6rem" style={{height: '8px'}}>
                                     <div className="bg-teal-500 h-full" style={{width: '40%'}}/>
                                 </div>
-                                <span className="text-teal-500 ml-3 font-medium">%40</span>
+                                <span className="text-teal-500 ml-3 font-medium">40%</span>
                             </div>
                         </li>
                     </ul>
